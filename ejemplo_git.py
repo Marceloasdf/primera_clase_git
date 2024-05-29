c5cee969eb9415c27475f0c811d52487d4d4f54e
marcelonovoa <ma.novoac@duocuc.cl>
2024-05-29 09:22:31 -0400

    cree mi diccionario del ejercicio

---
+++ ejemplo_git.py
@@ -0,0 +1,8 @@
+#ejercicio diccionario de hoy: usuario:
+diccionario = {
+    "identificador": 1,
+    "nombre": "marriott",
+    "direccion": "avenida Bernardo O'higgins 1123",
+    "funcionando": True,
+    "Valor habitacion": 100000
+}
